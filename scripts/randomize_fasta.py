2e1bda7176a16d7dfdf1b424d3fdad2853e7cadf
Didier Barradas Bautista <didier.barradas@gmail.com>
2021-11-23 13:21:45 +0300

    files added

--- scripts/randomize_fasta.py
+++ scripts/randomize_fasta.py
@@ -1,8 +1,8 @@
 ##https://stackoverflow.com/questions/31265282/how-to-randomly-extract-fasta-sequences-using-python
 from Bio import SeqIO
 from random import sample
-with open("/ibex/scratch/barradd/CO2FDH_22/files/blast_output_seq.fsa") as f:
+with open("../files/results.fsa") as f:
     seqs = SeqIO.parse(f, "fasta")
-    samps = ((seq.name, seq.seq) for seq in  sample(list(seqs),5000))
+    samps = ((seq.name, seq.seq) for seq in sample(list(seqs),5000))
     for samp in samps:
         print(">{}\n{}".format(*samp))
